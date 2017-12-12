98a9c36e0e7a9c270e50d2659e6f3dd7a5221df4
Sumeet Bhalla <sumeetbhalla@Sumeets-MacBook-Air.local>
2017-12-11 22:12:57 -0500

    post call

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .post('/messages',function(req, res) {
+    var str = req.body.message;
+    var jj = {};
+    var key = sha256(str);
+    jj["key"] = key;
+     res.send(JSON.stringify(jj));
+  })
+  .get('/messages/', function(req, res) {
+    res.render('pages/index')
+  })
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
